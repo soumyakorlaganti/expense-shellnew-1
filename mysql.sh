e8074ae5530f3dc4bde5381aea7c2f05db6e2eb5
soumya <korlagani.soumya@gmail.com>
2025-12-04 15:02:00 +0530

    expense

--- mysql.sh
+++ mysql.sh
@@ -79,7 +79,7 @@ then
     mysql_secure_installation --set-root-pass ExpenseApp@1
     VALIDATE $? "Setting UP root password"
 else
-    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOGFILE
+    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a &>>$LOGFILE
 fi
 
 # Assignment
