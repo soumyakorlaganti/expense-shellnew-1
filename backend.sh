64b5ce23574362e8ded9f8fb3a95457a6c3f25e6
soumya <korlagani.soumya@gmail.com>
2025-12-04 15:32:13 +0530

    expense

--- backend.sh
+++ backend.sh
@@ -5,9 +5,7 @@ source ./common.sh
 check_root
 
 dnf module disable nodejs -y &>>$LOG_FILE
-
 dnf module enable nodejs:20 -y &>>$LOG_FILE
-
 dnf install nodejs -y &>>$LOG_FILE
 
 id expense &>>$LOG_FILE
@@ -32,9 +30,7 @@ cp /home/ec2-user/expense-shellnew/backend.service /etc/systemd/system/backend.s
 
 
 systemctl daemon-reload &>>$LOG_FILE
-
 systemctl start backend &>>$LOG_FILE
-
 systemctl enable backend &>>$LOG_FILE
 
 dnf install mysql -y &>>$LOG_FILE
